6e4ddb87c4f4069ac86db797ec574a89acd7bc71
JonCabot <jfc0053@gmail.com>
2022-08-22 11:36:07 -0500

    added 5 more 8kyu

---
+++ 8kyu/EvenOrOdd.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+//My Solution
+function even_or_odd(number) {
+  if (number % 2 === 0)
+    return "Even"
+  else
+    return "Odd"
+}
+
+//Best Practice
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
---
+++ 8kyu/ReversedStrings.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+//My Solution
+function solution(str){
+  var splitString = str.split("");
+  var reverseArray =  splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+}
+
+//Best Practice
+function solution(str){
+  return str.split('').reverse().join('');  
+}
